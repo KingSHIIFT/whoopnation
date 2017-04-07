41ea933a8c03c8668a8cfa7405458e8e7008ee86
KingSHIIFT <KingSHIIFT@users.noreply.github.com>
2017-04-07 06:21:35 +0100

    Update index.js
    Added forecast command

--- index.js
+++ index.js
@@ -44,3 +44,59 @@ bot.on('chat', function(data) {
 		}
 	}
 });
+}
+      break;
+    case ".forecast":
+      if (qualifier === "") {
+        bot.sendChat("Try .forecast followed by a US state, city, or zip to look up.");
+      } else {
+        google_geocoding.geocode(qualifier, function(err, location) {
+          if (location !== null) {
+            weather.getWeather(location.lat, location.lng, function(err, data) {
+              if (data !== null) {
+                var weekForecast = "Forecast for " + data.location.areaDescription + ": Current: " + data.currentobservation.Temp + "°F " + data.currentobservation.Weather;
+                for (var i = 0; i < 7; i++) {
+                  var day = data.time.startPeriodName[i].split('');
+                  if (day[1] != 'Night') {
+                    weekForecast = weekForecast + "; " + data.time.startPeriodName[i] + ": ";
+                  } else {
+                    weekForecast = weekForecast + ", ";
+                  }
+                  weekForecast = weekForecast + data.time.tempLabel[i] + ": " + data.data.temperature[i] + "°F";
+                }
+                weekForecast = weekForecast.replace(/Sunday/g, 'Sun');
+                weekForecast = weekForecast.replace(/Monday/g, 'Mon');
+                weekForecast = weekForecast.replace(/Tuesday/g, 'Tues');
+                weekForecast = weekForecast.replace(/Wednesday/g, 'Wed');
+                weekForecast = weekForecast.replace(/Thursday/g, 'Thurs');
+                weekForecast = weekForecast.replace(/Friday/g, 'Fri');
+                weekForecast = weekForecast.replace(/Saturday/g, 'Sat');
+                bot.sendChat(weekForecast);
+              } else {
+                bot.sendChat("No weather has been found.");
+              }
+            });
+          } else {
+            bot.sendChat("No weather has been found.");
+          }
+        });
+      }
+      break;
+    case ".temp":
+      if (qualifier == "") {
+        bot.sendChat("Try .temp followed by a US state, city, or zip to look up.");
+      } else {
+        google_geocoding.geocode(qualifier, function(err, location) {
+          if (location != null) {
+            weather.getWeather(location.lat, location.lng, function(err, data) {
+              if (data != null) {
+                var temp = "Current temperature in " + data.location.areaDescription + ": " + data.currentobservation.Temp + "°F " + data.currentobservation.Weather;
+                bot.sendChat(temp);
+              } else {
+                bot.sendChat("No temperature has been found.");
+              }
+            });
+          } else {
+            bot.sendChat("No temperature has been found.");
+          }
+});
